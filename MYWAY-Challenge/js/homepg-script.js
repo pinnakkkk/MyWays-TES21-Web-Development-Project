d1b7f07d5cb85d4df6d04afd620aa3153a0d90e1
pinnakkkk <52191876+pinnakkkk@users.noreply.github.com>
2021-04-17 14:49:56 +0530

    Delete MYWAY-Challenge directory

--- MYWAY-Challenge/js/homepg-script.js
+++
@@ -1,9 +0,0 @@
-var angle = 0;
-
-var changeBackground = function () {
-	angle = angle + 3;
-
-	document.body.style.background	="linear-gradient("+ angle+"deg, #c9d6ff, #e2e2e2)"
-	requestAnimationFrame(changeBackground)
-}
-changeBackground()
